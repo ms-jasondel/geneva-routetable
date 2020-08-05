97e44abf0232f6f361fe0451c765d63f2e180765
Jason De Lorme <delormej@hotmail.com>
2020-08-05 17:32:06 -0400

    Updating deploy manifest.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
-
-# Create pull secret from ACR
-# NOT necesary if pulling image from dockerhub.
-# oc create secret docker-registry wthacr-secret \
-#     --docker-server=wthacr.azurecr.io \
-#     --docker-username=wthAcr \
-#     --docker-password='REDACTED' \
-#     --docker-email='noreply@azurecr.io'
-
 # Create project if it does not exist
 oc new-project geneva-route-table
 # OR Switch to project if it already exists.
 oc project geneva-route-table
 
+#
 # Copy over Azure credentials
+#
+# NOTE: this is just a cheat, make sure that the service principal has contributor rights
+# on the resource group, if it doesn't create another SP which has the appropriate permissions
+# and set the following keys; azure_client_id, azure_client_secret in the azure-credentials secret.
+#
 oc get secret azure-credentials --namespace=kube-system --export -o yaml |\
    oc apply -f -
 
